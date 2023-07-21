402869f5b75d5bf2b40bee059fee1f35dcfed157
paulocp14 <p.souza1411@gmail.com>
2023-07-21 14:46:12 -0300

    Projeto Conta Banco

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,14 @@
+public class ContaTerminal{
+    public static void main(String[]args) throws Exception {
+
+    int NumeroConta = 1021;
+    long agencia = 067-8;
+    String NomeCliente = "Paulo Souza";
+    float Saldo = 14000;
+
+    System.out.println("Cliente: " + NomeCliente);
+     System.out.println("Agencia: " + agencia);
+      System.out.println("Conta: " + NumeroConta);
+       System.out.println("Saldo: " + Saldo);
+    }
+}
